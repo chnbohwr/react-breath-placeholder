a7209b7869ab6f5e7172c18924bf06b7cbbc0bb3
Hyman_Chen <hyman_chen@wistron.com>
2018-05-31 15:46:18 +0800

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -2,11 +2,63 @@ import React, { Component } from 'react'
 
 import ExampleComponent from 'react-breath-placeholder'
 
+const style = {
+  container:{
+    padding: 20,
+  },
+  box1: {
+    width: 130,
+    height: 10,
+    marginBottom: 10
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    marginRight: 20
+  },
+  ph: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: 40
+  },
+  box3: {
+    width: 200,
+    height: 100
+  },
+  box4: {
+    width: 40,
+    height: 10
+  },
+  box5: {
+    height: 100,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    marginRight: 10
+  }
+};
+
 export default class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div style={style.container}>
+        <div style={style.ph}>
+          <ExampleComponent style={style.box2} />
+          <div>
+            <ExampleComponent style={style.box1} />
+            <ExampleComponent style={style.box1} />
+          </div>
+        </div>
+        <div style={style.ph}>
+          <div style={style.box5}>
+            <ExampleComponent style={style.box4} />
+            <ExampleComponent style={style.box4} />
+            <ExampleComponent style={style.box4} />
+            <ExampleComponent style={style.box4} />
+          </div>
+          <ExampleComponent style={style.box3} />
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,56 @@
-/**
- * @class ExampleComponent
- */
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import keyframes from "popmotion/animations/keyframes";
+import styler from "stylefire";
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+const defaultBoxStyle = {
+  width: 100,
+  height: 100
+};
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+export default class ShimBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.$box = React.createRef();
   }
-
+  static propTypes = {
+    loop: PropTypes.number,
+    backgrounds: PropTypes.arrayOf(PropTypes.string),
+    duration: PropTypes.number,
+    style: PropTypes.object,
+    className: PropTypes.string
+  };
+  static defaultProps = {
+    loop: Infinity,
+    backgrounds: ["#bbb", "#ddd", "#bbb"],
+    duration: 3000,
+    style: defaultBoxStyle,
+    className: ""
+  };
+  componentDidMount() {
+    const boxStyler = styler(this.$box.current);
+    const { loop, backgrounds, duration, style } = this.props;
+    this.keyframes = keyframes({
+      values: backgrounds.map(d => ({ backgroundColor: d })),
+      duration,
+      loop
+    }).start(keyStyle => {
+      const newStyle = { ...style, ...keyStyle };
+      boxStyler.set(newStyle);
+    });
+  }
+  componentWillUnmount() {
+    this.keyframes.stop();
+  }
   render() {
-    const {
-      text
-    } = this.props
-
+    const props = this.props;
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+      <div
+        ref={this.$box}
+        className={classnames("placeholder-box", props.className)}
+        style={{ ...defaultBoxStyle, ...props.style }}
+      />
+    );
   }
 }
